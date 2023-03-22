c8d28463338f9fd1223244c5cfa07eb604c3ace1
Oliver Michaelsen <56824279+omicca@users.noreply.github.com>
2023-03-22 16:51:18 +0100

    visualization update

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pickle as pk
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from sklearn.ensemble import IsolationForest
+import visualization as vs
 
+# convert to csv and load data
 with open('data/Credit Card Fraud Detection_train.pkl', "rb") as file:
     data = pk.load(file)
 dataframe = pd.DataFrame(data)
 dataframe.to_csv(r'csv-data/file.csv')
+dataframe = pd.read_csv('csv-data/file.csv')
 
-datafrane = pd.read_csv('csv-data/file.csv')
-
-print(datafrane.info('csv-data/file.csv'))
+vs.visualize_data()
+plt.show()
