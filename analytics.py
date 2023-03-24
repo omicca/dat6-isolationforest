1da49e3dc74c275eb0296d12427b1964eba96240
Oliver Michaelsen <56824279+omicca@users.noreply.github.com>
2023-03-24 15:59:50 +0100

    Menu

--- analytics.py
+++ analytics.py
@@ -2,22 +2,33 @@ import pandas as pd
 import pickle as pk
 import seaborn as sns
 
-data = pd.read_csv("csv-data/file.csv")
-def dataset_analytics():
-    #average
-    average = (data['28'].mean())
-    print("Average transaction amount: " + str(average))
+#data = pd.read_csv("csv-data/file.csv")
 
-    #contains null
-    if (data.isnull == True):
-        print("data contains null values")
+
+def dataset_analytics(dataframe):
+    # average transaction
+    mean = (dataframe['28'].mean())
+    print("Average transaction amount: " + str(mean))
+
+    median = (dataframe['28'].median())
+    print("Middle value: " + str(median))
+
+
+    # contains null
+    if (dataframe.isnull == True):
+        print("Null values: True\n\n")
     else:
-        print("data contains no null values")
+        print("Null values: False\n\n")
+
+
+
 
 def pkl_to_csv(datafile):
     # convert to csv
-    with open('data/Credit Card Fraud Detection_train.pkl', "rb") as file:
+    with open('data/Credit Card Fraud Detection_' + datafile + '.pkl', "rb") as file:
         data = pk.load(file)
     dataframe = pd.DataFrame(data)
     dataframe.to_csv(r'csv-data/file.csv')
     dataframe = pd.read_csv('csv-data/file.csv')
+
+    return dataframe
--- main.py
+++ main.py
@@ -7,6 +7,41 @@ from sklearn.ensemble import IsolationForest
 import visualization as vs
 import analytics as an
 
-x = input("1. test\n2. testlabel\n3. train\nSelection: ")
+select = input("1. Convert .pkl\n2. Dataset analytics\n3. Data visualization\nSelection: ")
 
-an.dataset_analytics()
+while (True):
+    if (select == "1"):
+        x = input("1. test\n2. testlabel\n3. train\nSelection: ")
+        match x:
+            case "1":
+                df = an.pkl_to_csv("test")
+
+            case "2":
+                df = an.pkl_to_csv("testlabel")
+
+            case "3":
+                df = an.pkl_to_csv("train")
+
+            case other:
+                print("Invalid selection")
+                exit()
+
+
+        print(".pkl converted successfully.\n\n")
+
+    elif (select == "2"):
+        try:
+            an.dataset_analytics(df)
+        except NameError:
+            print("ERROR: Convert .pkl file before accessing dataframe\n")
+
+    elif (select == "3"):
+        print("test")
+
+    else:
+        print("Invalid selection")
+        exit()
+
+    select = input("1. Convert .pkl\n2. Dataset analytics\n3. Data visualization\nSelection: ")
+
+
